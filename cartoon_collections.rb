48b1dff648653274f8fe6f293e4823549562b29d
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-06 18:41:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,13 @@ def summon_captain_planet(array)
   array.collect {|power| "#{power.capitalize()}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.all? do |four_or_more|
+    if four_or_more.length > 4
+      true 
+    else 
+      false 
+    end 
 end
 
 def find_the_cheese# code an argument here
