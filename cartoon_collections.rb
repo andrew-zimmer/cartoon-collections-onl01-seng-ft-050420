c1d3beb75e546d67076e9671b4f48a9ab2aeb409
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-06 18:33:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.select! {|power| "#{power.capitalize()}!"}
+  array.select {|power| "#{power.capitalize()}!"}
 end
 
 def long_planeteer_calls# code an argument here
